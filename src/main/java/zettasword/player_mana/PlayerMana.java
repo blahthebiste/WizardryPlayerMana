aba3f3767b9211e709b731f79f457db508528d29
blahthebiste <eliezermiron116@gmail.com>
2025-12-17 15:08:27 -0800

    Updated version string to 1.2.1.

--- src/main/java/zettasword/player_mana/PlayerMana.java
+++ src/main/java/zettasword/player_mana/PlayerMana.java
@@ -19,7 +19,7 @@ import zettasword.player_mana.proxy.CommonProxy;
 public class PlayerMana {
     public static final String MODID = "player_mana";
     public static final String NAME = "Player Mana";
-    public static final String VERSION = "1.2.0";
+    public static final String VERSION = "1.2.1";
 
     @SidedProxy(clientSide = "zettasword.player_mana.proxy.ClientProxy",
             serverSide = "zettasword.player_mana.proxy.CommonProxy")
